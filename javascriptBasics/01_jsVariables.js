b48d8c3efcf47feaaab3e7128e704eb9b83fdee2
adeelahmadcoder <adeelahmadkhanak47@gmail.com>
2024-09-18 19:05:52 +0000

    changes

---
+++ javascriptBasics/01_jsVariables.js
@@ -0,0 +1,15 @@
+const customerId=1234;
+let customer_Name="Adeel";
+var Customer_State="";
+
+let custobj={
+    customer_Name,
+    customerId,
+    Customer_State,
+    
+}
+console.log(custobj)
+//code should be proper readable 
+//let scope just in the block 
+//const variable value cannot be changed or overrwrite 
+//var scope all over the file  
